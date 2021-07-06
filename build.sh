85ebbf31e34188ae44cd066039d58e6f0cade61e
Cristian Garcia <cgarcia.e88@gmail.com>
2021-07-05 19:41:44 -0500

    update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-python -m streambook.gen main.py > main.notebook.py
+python -m streambook main.py --nowatch
 jupytext --to notebook --execute main.notebook.py --output main.ipynb
 jupytext --to markdown --execute main.notebook.py --output README.md # use nbconvert instead to save computation
 poetry export -f requirements.txt --output requirements.txt
