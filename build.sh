1a1b9b8cb12ac61fef230affdf62bb1b3bef017b
Cristian Garcia <cgarcia.e88@gmail.com>
2021-07-16 20:14:32 -0500

    rename files

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 python -m streambook main.py --nowatch
-jupytext --to notebook --execute main.notebook.py --output main.notebook.ipynb
-jupyter nbconvert --to markdown --output README.md main.notebook.ipynb
+jupytext --to notebook --execute main.notebook.py --output main.ipynb
+jupyter nbconvert --to markdown --output main.md main.ipynb
+cp main.md README.md
 poetry export -f requirements.txt --output requirements.txt
